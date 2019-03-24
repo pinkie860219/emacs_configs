edca56c193aec3ea14cbb1436437d47f85d73eae
yinghua <shadow10230@gmail.com>
2019-03-24 03:20:45 +0000

    ikjl instead of arrow key

--- init.el
+++ init.el
@@ -23,14 +23,19 @@
 (show-paren-mode 1)
 (setq show-paren-delay 0)
 
+;;括號自動完成
+(electric-pair-mode 1)
+
 ;;取消自動備份
 (setq make-backup-files nil) ; stop creating backup~ files
 
 
 ;;smooth scrolling
-(setq scroll-step            1
-      scroll-conservatively  10000)
-
+;;(setq scroll-step            1
+;;      scroll-conservatively  10000)
+(require 'smooth-scrolling)
+(smooth-scrolling-mode 1)
+(setq smooth-scroll-margin 5)
 
 ;;buffer 管理器
 (require 'ibuffer)
@@ -49,6 +54,16 @@
 (global-set-key (kbd "<f11>") (lambda () (interactive) (other-frame 1)))
 (global-set-key (kbd "<f12>") (lambda () (interactive) (other-frame 1)))
 
+;;改方向鍵和字詞移動
+(global-set-key (kbd "M-i") 'previous-line)
+(global-set-key (kbd "M-k") 'next-line)
+(global-set-key (kbd "M-j") 'left-char)
+(global-set-key (kbd "M-l") 'right-char)
+(global-set-key (kbd "M-I") 'backward-paragraph)
+(global-set-key (kbd "M-K") 'forward-paragraph)
+(global-set-key (kbd "M-J") 'left-word)
+(global-set-key (kbd "M-L") 'right-word)
+(global-set-key (kbd "C-o") 'recenter-top-bottom)
 
 ;;顏色主題設定
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
@@ -61,11 +76,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("a56cc18045d90be8f770ae409fc86274f8e5de2999a16b604ff84f8015e8d1e5" "3b36631f95ebfd9ec35f382249ad861f3b3d51f8bed4882184ec8745deaada28" default)))
+    ("151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" "93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "e61752b5a3af12be08e99d076aedadd76052137560b7e684a8be2f8d2958edc3" "a12ec87ff9e72a9561314c7ae2c82a373e1b7c80d0fe15579e282080c8d5aef2" "0dd2666921bd4c651c7f8a724b3416e95228a13fca1aa27dc0022f4e023bf197" "a56cc18045d90be8f770ae409fc86274f8e5de2999a16b604ff84f8015e8d1e5" "3b36631f95ebfd9ec35f382249ad861f3b3d51f8bed4882184ec8745deaada28" default)))
  '(org-agenda-files (quote ("~/Desktop/todo.org")))
  '(package-selected-packages
    (quote
-    (magit go-mode rainbow-delimiters js2-mode rjsx-mode auto-complete golden-ratio))))
+    (sr-speedbar material-theme smooth-scrolling magit go-mode rainbow-delimiters js2-mode rjsx-mode auto-complete golden-ratio))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -76,9 +91,17 @@
 
 ;;;;;;;program;;;;;;;;;;;;
 
+;;space istead of tab
+(setq-default indent-tabs-mode nil)
+
 ;;設定c語言的縮排為4個空白
 (setq c-default-style "linux"
       c-basic-offset 4)
+
+;;js2-mode
+(add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
+(setq-default js2-basic-offset 2)
+(setq js-indent-level 2)
 
 ;;;;;;;/program;;;;;;;;;;;;
 
@@ -92,5 +115,5 @@
 
 ;;;;;;;rc-config;;;;;;;;;;;;
 (add-to-list 'load-path "~/.emacs.d/rc")
-(require 'rc-open-todo)
+;;(require 'rc-open-todo)
 ;;;;;;/rc-config;;;;;;;;;;;;
